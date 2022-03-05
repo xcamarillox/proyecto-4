2c3d539a24c73888d2a8d9f03ac14d74254d156f
CARLOS CAMARILLO <carlos.adrian.camarillo@gmail.com>
2022-03-05 13:46:24 -0600

    package.json, index.html, index.js: updated. Bootstrap working

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.css';
 import App from './App.jsx';
 
