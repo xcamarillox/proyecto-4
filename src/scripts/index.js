faad30031cc696725f9a710a70e6bdba2c399836
CARLOS CAMARILLO <carlos.adrian.camarillo@gmail.com>
2022-03-04 19:11:30 -0600

    index.js: updated. react router added on App. Router imported on index.js.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import '../styles/index.css';
 import App from './App.jsx';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
